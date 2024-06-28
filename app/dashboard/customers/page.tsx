85d70dfc46e19a30477226c1d6e8f111a5a98237
Terry Lee <terry.lee@avb.net>
2024-06-28 15:52:02 -0700

    finish customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Metadata } from 'next';
+import CustomersTable from '@/app/ui/customers/table';
+import { TableRowSkeleton } from '@/app/ui/skeletons';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-const page = () => {
-  return <div>Customer Page</div>;
-};
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
 
-export default page;
+  return (
+    <Suspense fallback={<TableRowSkeleton />}>
+      <CustomersTable query={query} />;
+    </Suspense>
+  );
+}
